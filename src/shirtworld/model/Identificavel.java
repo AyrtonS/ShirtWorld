60a60d9679d3e6b50c3bf395dd1a387ba0906078
marvin3301 <viniips@hotmail.com>
2017-11-21 21:35:09 -0300

    desenvolvimento

---
+++ src/shirtworld/model/Identificavel.java
@@ -0,0 +1,19 @@
+package shirtworld.model;
+
+public class Identificavel {
+
+	private int id;
+
+	public Identificavel() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
--- src/shirtworld/model/Produto.java
+++ src/shirtworld/model/Produto.java
@@ -1,7 +1,29 @@
 package shirtworld.model;
 
-public class Produto {
-	
-	
-		
+public class Produto extends Identificavel{
+
+	private String nome;
+	private float preco;
+
+	public Produto(String nome, float preco) {
+		setNome(nome);
+		setPreco(preco);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
 }
